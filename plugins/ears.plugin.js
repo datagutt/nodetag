6eda53d5420e86314c805edfcc479f89d5097472
datagutt <datagutt666@hotmail.com>
2012-05-02 19:34:20 +0200

    ear fix

--- plugins/ears.plugin.js
+++ plugins/ears.plugin.js
@@ -5,8 +5,8 @@ exports.init = function(plugins, server){
 		var doc = args.doc;
 		var ambient = args.ambient;
 		if(doc && doc.action && doc.action == 'ears' && doc.left && doc.right){
-			encode.left_ear(ambient, doc.left);
-			encode.right_ear(ambient, doc.right);
+			encode.left_ear(ambient, parseInt(doc.left, 10));
+			encode.right_ear(ambient, parseInt(doc.right, 10));
 			server.isAmbient = 1;
 		}
 	});
