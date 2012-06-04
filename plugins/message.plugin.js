fa683f7c4e7110e8c20deef61e0b5ec8e28130df
datagutt <datagutt666@hotmail.com>
2012-06-04 21:23:40 +0200

    small formatting change

--- plugins/message.plugin.js
+++ plugins/message.plugin.js
@@ -5,7 +5,8 @@ exports.init = function(plugins, server){
 		var data = args.data;
 		var ambient = args.ambient;
 		var message = [];
-		message.push('ID ' + Math.floor((new Date).getTime() / 1000) + '\n');		//message.push('ST /mood/1.mp3');
+		message.push('ID ' + Math.floor((new Date).getTime() / 1000) + '\n');	
+		//message.push('ST /mood/1.mp3');
 		message.push('MU http://translate.google.com/translate_tts?q=A+simple_text+to+voice+demonstration');
 		message = message.join('\n');
 		if(doc && doc.action && doc.action == 'tts'){
